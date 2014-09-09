5778dbd1acaffcb6ba7a3136af9d01b409a7e4dd
guibs <guillaume.baudusseau@gmail.com>
2014-09-09 16:59:16 +0200

    add isHostBlacklisted tests (mocha)

--- test/test.js
+++ test/test.js
@@ -2,23 +2,23 @@ var suich = require('..'),
 	assert = require('assert'),
 	Q = require('q');
 
-describe('safe url input checker library for node.js', function() {
+describe('safe url input checker library for node.js ', function() {
 
 	describe('getIPAddresses() method', function() {
-		it('should get IP addresses', function(done) {
+		it('#1 should get IP addresses', function(done) {
 			suich.getIPAddresses('w3.org', function(err, res) {
 				assert.equal(res, "128.30.52.45");
 				done();
 			});
 		});
-		it('should get IP addresses', function(done) {
+		it('#2 should get IP addresses', function(done) {
 			suich.getIPAddresses('localhost', function(err, res) {
 				assert.equal(res, "127.0.0.1");
 				done();
 			});
 		});
-		it('should not get IP addresses', function(done) {
-			suich.getIPAddresses('foofoofoofoofoo.com', function(err, res) {
+		it('#3 should not get IP addresses', function(done) {
+			suich.getIPAddresses('foofoofoo.com', function(err, res) {
 				assert.ifError(res);
 				done();
 			});
@@ -27,11 +27,30 @@ describe('safe url input checker library for node.js', function() {
 
 
 	describe('isHostBlacklisted() method', function() {
-		it('should return true', function() {
-			suich.isHostBlacklisted('google.com', ['google.com', 'pinpin.com'], function(err, res) {
+		it('#1 should return true', function() {
+			suich.isHostBlacklisted('google.com', ['google.com', 'w3.org'], function(err, res) {
 				assert.equal(res, true);
 			});
 		});
+		it('#2 should return false', function() {
+			suich.isHostBlacklisted('foofoofoo.com', ['google.com', 'w3.org'], function(err, res) {
+				assert.equal(res, false);
+			});
+		});
+		it('#3 should return false', function() {
+			suich.isHostBlacklisted('foofoofoo.com', [], function(err, res) {
+				assert.equal(res, false);
+			});
+		});
+		it('#4 should return true', function() {
+			assert.equal(suich.isHostBlacklisted('google.com', ['google.com', 'w3.org']), true);
+		});
+		it('#5 should return false', function() {
+			assert.equal(suich.isHostBlacklisted('foofoofoo.com', ['google.com', 'w3.org']), false);
+		});
+		it('#6 should return false', function() {
+			assert.equal(suich.isHostBlacklisted('foofoofoo.com', []), false);
+		});
 	});
-	
+
 });
